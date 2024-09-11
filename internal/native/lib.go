e0e97d5b9537521562fd60c46b025a4cfa90884a
Yousaf Nabi <you@saf.dev>
2024-09-11 23:30:25 +0100

    test: use forked examples, darwin amd64 import

--- internal/native/lib.go
+++ internal/native/lib.go
@@ -3,7 +3,7 @@ package native
 
 /*
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/libs/macos-aarch64 -L/tmp -L/usr/local/lib -Wl,-rpath -Wl,/tmp -Wl,-rpath -Wl,${SRCDIR}/libs/macos-aarch64 -Wl,-rpath -Wl,/usr/local/lib -lpact_ffi
-#cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/libs/macos-x86_64 -L/tmp -L/usr/local/lib -Wl,-rpath -Wl,/tmp -Wl,-rpath -Wl,${SRCDIR}/libs/macos-x86_64 -Wl,-rpath -Wl,/usr/local/lib -lpact_ffi
+#cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/libs/macos-x86_64 -L/tmp -L/usr/local/lib -Wl,-rpath -Wl,/tmp -Wl,-rpath -Wl,${SRCDIR}/libs/macos-x86_64 -Wl,-rpath -Wl,/usr/local/lib -lpact_ffi
 #cgo windows,amd64 LDFLAGS: -L${SRCDIR}/libs/windows-x86_64 -lpact_ffi
 #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/libs/linux-aarch64 -L/tmp -L/opt/pact/lib -L/usr/local/lib -Wl,-rpath -Wl,${SRCDIR}/libs/linux-aarch64 -Wl,-rpath -Wl,/opt/pact/lib -Wl,-rpath -Wl,/tmp -Wl,-rpath -Wl,/usr/local/lib -lpact_ffi
 #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/libs/linux-x86_64 -L/tmp -L/opt/pact/lib -L/usr/local/lib -Wl,-rpath -Wl,${SRCDIR}/libs/linux-x86_64 -Wl,-rpath -Wl,/opt/pact/lib -Wl,-rpath -Wl,/tmp -Wl,-rpath -Wl,/usr/local/lib -lpact_ffi
